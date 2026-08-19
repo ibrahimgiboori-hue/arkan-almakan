e5fa6e8b3aacd9bef195a785fe125524715e90bc
ibrahimgiboori-hue <ibrahimgiboori@gmail.com>
2026-08-19 09:46:18 +0300

    Brand public careers pages professionally

---
+++ app/careers/layout.js
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: 'فرص العمل | أركان المكان للمقاولات',
+  description: 'الفرص الوظيفية والتقديم للعمل لدى أركان المكان للمقاولات',
+};
+
+export default function CareersLayout({ children }) {
+  return children;
+}
