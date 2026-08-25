135e6287571d20e5fd1f7bb06a92096a8253cc76
ibrahimgiboori-hue <ibrahimgiboori@gmail.com>
2026-08-25 15:20:26 +0300

    refactor(v2): mount print hardening constitution globally

--- app/print/layout.js
+++ app/print/layout.js
@@ -1,5 +1,6 @@
 import './print-system.css';
 import './procedure-system.css';
+import './print-constitution-hardening.css';
 import PrintGovernanceBoundary from '@/components/print/PrintGovernanceBoundary';
 
 export default function PrintLayout({ children }) {
