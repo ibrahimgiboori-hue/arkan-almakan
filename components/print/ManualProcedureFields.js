7c86c2b7ca0fde984ac13cb5769a906126861267
ibrahimgiboori-hue <ibrahimgiboori@gmail.com>
2026-08-28 21:03:40 +0300

    Use full-width symmetric manual signature lines

--- components/print/ManualProcedureFields.js
+++ components/print/ManualProcedureFields.js
@@ -1,14 +1,14 @@
 export default function ManualProcedureFields() {
   return (
     <div className="procedure-signature-fields">
-      <div className="procedure-signature-date">
-        <span>التاريخ:</span>
-        <span className="procedure-signature-date-dots" aria-hidden="true">
-          ..........................................
-        </span>
+      <div className="procedure-signature-field">
+        <span>التوقيع:</span>
+        <span className="procedure-signature-line" aria-hidden="true" />
       </div>
-      <div className="procedure-signature-handwriting-space" aria-hidden="true" />
-      <div className="procedure-signature-sign">التوقيع:</div>
+      <div className="procedure-signature-field">
+        <span>التاريخ:</span>
+        <span className="procedure-signature-line" aria-hidden="true" />
+      </div>
     </div>
   );
 }
