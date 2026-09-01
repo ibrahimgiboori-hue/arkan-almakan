075274037902bf8a5d35366652019a0cfa8ebfe5
ibrahimgiboori-hue <ibrahimgiboori@gmail.com>
2026-09-01 17:48:48 +0300

    Load governed project report paper-form styling

--- app/print/layout.js
+++ app/print/layout.js
@@ -2,6 +2,7 @@ import './print-constitution.css';
 import './print-office-model.css';
 import './print-generic-governance.css';
 import './print-blank-form.css';
+import './print-report-paper-form.css';
 import PrintGovernanceBoundary from '@/components/print/PrintGovernanceBoundary';
 
 export default function PrintLayout({ children }) {
