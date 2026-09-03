ae9dc36b4de07388779c9798248561e33791de6e
ibrahimgiboori-hue <ibrahimgiboori@gmail.com>
2026-09-03 22:05:30 +0300

    Isolate print routes behind a single scoped root

--- app/print/layout.js
+++ app/print/layout.js
@@ -3,8 +3,13 @@ import './print-office-model.css';
 import './print-generic-governance.css';
 import './print-blank-form.css';
 import './print-report-paper-form.css';
+import './print-register-report.css';
 import PrintGovernanceBoundary from '@/components/print/PrintGovernanceBoundary';
 
 export default function PrintLayout({ children }) {
-  return <PrintGovernanceBoundary>{children}</PrintGovernanceBoundary>;
+  return (
+    <div className="print-route-root" data-print-route-root="true">
+      <PrintGovernanceBoundary>{children}</PrintGovernanceBoundary>
+    </div>
+  );
 }
