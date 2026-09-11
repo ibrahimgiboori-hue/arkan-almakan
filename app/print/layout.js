7546e8aaa0defabb1a53638e62b4188f103bc674
ibrahimgiboori-hue <ibrahimgiboori@gmail.com>
2026-09-11 21:47:53 +0300

    feat(print): load unified side-rail workbench

--- app/print/layout.js
+++ app/print/layout.js
@@ -4,6 +4,7 @@ import './print-generic-governance.css';
 import './print-blank-form.css';
 import './print-report-paper-form.css';
 import './print-register-report.css';
+import './print-workbench.css';
 import PrintGovernanceBoundary from '@/components/print/PrintGovernanceBoundary';
 
 export default function PrintLayout({ children }) {
